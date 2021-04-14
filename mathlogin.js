705080510deda48fa78c4095290de11890f32b3d
Arav2009 <80956843+Arav2009@users.noreply.github.com>
2021-04-14 12:05:58 +0530

    Add files via upload

---
+++ mathlogin.js
@@ -0,0 +1,10 @@
+function login()
+{
+    player1_name=document.getElementById("player1_name_input").value;
+    player2_name=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player_1=", player1_name);
+    localStorage.setItem("player_2=", player2_name);
+
+    window.location="game.html";
+}
